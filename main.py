e68982ffecbdcc4968b564c68cc76dc32f5b8552
genuinely_sane <panditamey1@gmail.com>
2024-06-12 12:20:46 +0530

    code to get numbers from dict

--- main.py
+++ main.py
@@ -33,7 +33,10 @@ if st.button("Show Top 5 Numbers"):
 # add number to follow_counts
 if st.button("Add number to follow_counts"):
     number = st.number_input("Enter number to add:", min_value=0, max_value=36, step=1)
-    follow_counts[number] = defaultdict(int)
+    number = str(number)
+    number2add = st.number_input("Enter number to add:", min_value=0, max_value=36, step=1)
+    number2add = str(number2add)
+    follow_counts[number].append(number2add)
     st.write(f"Number {number} added to follow_counts")
     with open('follow_counts.json', 'w') as f:
         json.dump(follow_counts, f)
